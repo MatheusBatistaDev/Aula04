ea01f7eb94676a6f1b316697d918486f856f8f54
Matheus Batista <dev.batista.matheus@gmail.com>
2025-07-10 17:57:32 -0300

    adicionando exmp lista

---
+++ exemplo_lista.py
@@ -0,0 +1,12 @@
+produto: str = "sapato"
+produto_2: str = "camisa"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+print(produtos)
+
